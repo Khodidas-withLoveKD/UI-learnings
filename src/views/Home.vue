d03b1cc9ebd599e04bfe885a6d462d45eec0c6ac
Khodidas Chauhan <chauhan.khodidas@gmail,com>
2021-03-03 11:59:15 +0530

    Learning Draggable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
     <h3><router-link to="/nestedroutes">Nested Routing Example</router-link></h3>
     <h3><router-link to="/testing">Few Tests Here !!!</router-link></h3>
     <h3><router-link to="/grid-view">Grid View Responsive Site !!!</router-link></h3>
+    <h3><router-link to="/watch-vs-computed">Watch Vs. Computed</router-link></h3>
+    <h3><router-link to="/drragable-div">Drag and Drop</router-link></h3>
+    <h3><router-link to="/transitions">Transitions</router-link></h3>
   </div>
 </template>
 
