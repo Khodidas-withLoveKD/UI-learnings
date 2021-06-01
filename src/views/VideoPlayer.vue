27ad7314138d1988d2d6cfee7f8527d209575ed2
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2021-06-01 17:50:10 +0530

    The button finally started working though there are some minor changes

--- src/views/VideoPlayer.vue
+++ src/views/VideoPlayer.vue
@@ -2,15 +2,10 @@
     <div>
         <!-- <video ref="videoPlayer" class="video-js" data-setup="videoOptions"> -->
         <!-- <video ref="videoPlayer" class="video-js" :options="playerOptions"> -->
-        <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered"
-          data-setup='{
-            "controls": true,
-            "width": 1000,
-            "height": 600,
-            "preload": "metadata"
-            }'>
+        <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered">
           <!-- <source src="@/assets/video.mp4" type="video/mp4"> -->
-          <source src="https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8" type="application/x-mpegURL">
+          <!-- <source src="https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8" type="application/x-mpegURL"> -->
+          <source src="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8" type="application/x-mpegURL">
         </video>
     </div>
 </template>
@@ -34,7 +29,14 @@ export default {
     return {
       player: null,
       playerOptions: {
-        controls: true
+        controls: true,
+        height: 540,
+        width: 750,
+        plugins: {
+          httpSourceSelector: {
+            default: 'auto'
+          }
+        }
       }
     }
   },
@@ -49,70 +51,54 @@ export default {
   },
   mounted () {
     require('videojs-contrib-quality-levels')
-    require('videojs-hls-quality-selector')
+    // require('videojs-hls-quality-selector')
+    require('videojs-http-source-selector') // Symbol nano che baaki works great, too many console.logs
+    // require('videojs-selector-quality-hls')
+    // require('videojs-max-quality-selector') // does not work
 
-    this.player = videojs(this.$refs.videoPlayer, function onPlayerReady () {
-      console.log('onPlayerReady', this)
-    })
-    if (this.player.ready()) {
-      console.log('REASDY')
-      this.player.play()
-    }
-    const controlBar = this.player.getChild('ControlBar')
-    controlBar.removeChild(controlBar.getChild('SubsCapsButton'))
-    this.player.hlsQualitySelector({
-      displayCurrentQuality: true
-      // vjsIconClass: 'vjs-icon-cog'
-    })
-    this.player.muted(true)
-    // console.log('controlBar:', controlBar)
-    console.log('Plater = ', this.player)
-    let qualityLevels = this.player.qualityLevels()
-    // qualityLevels.removeQualityLevel()
-    // this.levels_.splice(i, 1)[0]
-    console.log('qLevels:', qualityLevels)
-    console.log('qLevels LENGTH:', qualityLevels.length)
-    console.log('qualityLevels.levels_:', typeof qualityLevels.levels_)
-    // console.log('qualityLevels.levels_:', qualityLevels.levels_.splice(0, 1)[0])
+    this.player = videojs(this.$refs.videoPlayer, this.playerOptions, function onPlayerReady () {
+      const controlBar = this.getChild('ControlBar')
+      controlBar.removeChild(controlBar.getChild('SubsCapsButton'))
+      console.log('CONTROLBAR = ', controlBar)
+      this.muted(true)
+      // ============= TimeStamp ====================
+      this.currentTime(24)
+      // this.maxQualitySelector()
+      this.httpSourceSelector() // Symbol nano che baaki works great, too many console.logs
+      console.log('onPlayerR READDYDYDYDYDYDY', this)
+      this.on('loadedmetadata', function () {
+        console.log('DURATION = ', this.duration())
+        const qualityLevels = this.qualityLevels()
+        qualityLevels.removeQualityLevel(qualityLevels[4])
+        qualityLevels.removeQualityLevel(qualityLevels[1])
+        // ================== Quality Thing ======================
+        // ==== Silvermine =======
+        // ====== HLS ========
+        //   const qualityLevels = this.qualityLevels()
+        //   console.log('qualityLevels:', qualityLevels)
+        //   console.log('=== PLAYER READY ====')
+        //   console.log('qualityLevels2:', qualityLevels)
+        //   qualityLevels.removeQualityLevel(qualityLevels[4])
+        //   qualityLevels.removeQualityLevel(qualityLevels[0])
+        //   // removeQualityLevel(qualityLevels[4])
+        //   // this.qualityLevels = qualityLevels
+        //   // console.log('YOYOYOqualityLevels2:', this.qualityLevels)
+        //   qualityLevels.on('addqualitylevel', function (event) {
+        //     const qualityLevel = event.qualityLevel
 
-    const levels = []
-    qualityLevels.on('addqualitylevel', function (event) {
-      const qualityLevel = event.qualityLevel
-      console.log('qualityLevel:', qualityLevel)
-      if (qualityLevel.height <= 720) {
-        // this.player.qualityLevels().levels_.push(qualityLevel)
-      }
+      //     if (qualityLevel.height >= 720) {
+      //       qualityLevel.enabled = true
+      //     } else {
+      //       qualityLevel.enabled = false
+      //     }
+      //   })
+      //   this.hlsQualitySelector({
+      //     displayCurrentQuality: true
+      //   // vjsIconClass: 'vjs-icon-cog'
+      //   })
+      // })
+      })
     })
-    console.log('AFTER =============')
-    qualityLevels = levels
-    this.player.qualityLevels().removeQualityLevel(720)
-    console.log('Levels:', levels)
-    console.log('qualityLevel:', qualityLevels)
-    console.log('qLevels:', this.player.qualityLevels())
-    console.log('qLevels LENGTH:', qualityLevels.length)
-    console.log('qualityLevels.levels_:', typeof qualityLevels.levels_)
-    // console.log('qualityLevels.levels_:', qualityLevels.levels_.splice(0, 1)[0])
-    // for (const quality of qualityLevels) {
-    //   console.log('quality:', quality)
-    //   if (quality.height > 720) {
-    //     quality.enabled = false
-    //   }
-    // }
-    // qualityLevels.forEach(quality => {
-    //   console.log('quality:', quality)
-    //   if (quality.height > 720) {
-    //     quality.enabled = false
-    //   }
-    // })
-    // qualityLevels.on('addqualitylevel', function (event) {
-    //   const qualityLevel = event.qualityLevel
-
-    //   if (qualityLevel.height <= 720) {
-    //     qualityLevel.enabled = true
-    //   } else {
-    //     qualityLevel.enabled = false
-    //   }
-    // })
   },
   beforeDestroy () {
     if (this.player) {
